16d77bef4108b74ac86cfbdc5ab411a8ac0dafc5
BenoitMiquey <benoit.miquey@etu.univ-lyon1.Fr>
2019-01-28 16:39:49 +0100

    ajout exemple de Dao.php

---
+++ php/models/exempleDB.php
@@ -0,0 +1,21 @@
+<?php
+
+	require_once(PATH_MODELS.'DAO.php');
+	class exemple extends DAO{
+		
+		// Retourne un tableau 2d( avec plusieurs rows, d'ou l'utilisation de queryAll )
+		public function getListeCat(){
+			return DAO::queryAll('select nomCategorie from Categorie');
+		}
+		// Retourne un tableau 1d ( avec une seule ligne, d'ou l'utilisation de queryRow ) 
+		public function getNbCat(){
+			return DAO::queryRow('select count(*) from Categorie');
+		}
+		// Ne retourne pas de tableau, a utiliser pour les requètes de type Update, Delete
+		public function ajouterPhoto($nomFichier,$description,$catId){
+	
+			return DAO::queryBdd('INSERT INTO Photo (photoId, nomFichier, description, catId) VALUES (NULL,?,?,?)',[$nomFichier,$description,$catId]);
+		}
+	}
+?>
+
