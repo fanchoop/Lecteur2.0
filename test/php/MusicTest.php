5668c0b5e30da804c0ad6d7e9faf84e9b633214a
laurent <djidane56@gmail.com>
2019-01-29 14:40:43 +0100

    fin MusicTest

--- test/php/MusicTest.php
+++ test/php/MusicTest.php
@@ -75,5 +75,106 @@ final class MusicTest extends TestCase
         $this->assertSame("Autre artiste", self::$music->getArtist());
 
     }
-    
+
+    /**
+     * @cover ::getGenre
+     */
+    public function testGetGenre(){
+
+        $this->assertSame([], self::$music->getGenre());
+    }
+
+    /**
+     * @cover ::setGenre
+     */
+    public function testSetGenre(){
+
+        self::$music->setGenre("newGenre");
+        $this->assertSame(["newGenre"], self::$music->getGenre());
+
+    }
+
+    /**
+     * @cover ::getAnnee
+     */
+    public function testGetAnnee(){
+
+        $this->assertSame(0, self::$music->getAnnee());
+    }
+
+    /**
+     * @cover ::setAnnee
+     */
+    public function testSetAnnee(){
+
+        self::$music->setAnnee(1990);
+        $this->assertSame(1990, self::$music->getAnnee());
+
+    }
+
+    /**
+     * @cover ::getLike
+     */
+    public function testGetLike(){
+
+        $this->assertSame(0, self::$music->getLike());
+    }
+
+    /**
+     * @cover ::setLike
+     */
+    public function testSetLike(){
+
+        self::$music->setLike(42);
+        $this->assertSame(42, self::$music->getLike());
+
+    }
+
+    /**
+     * @cover ::addLike
+     */
+    public function testAddLike(){
+
+        self::$music->addLike();
+        $this->assertSame(43, self::$music->getLike());
+
+    }
+
+    /**
+     * @cover ::deleteLike
+     */
+    public function testDeleteLike(){
+
+        self::$music->deleteLike();
+        $this->assertSame(42, self::$music->getLike());
+
+    }
+
+    /**
+     * @cover ::getView
+     */
+    public function testGetView(){
+
+        $this->assertSame(0, self::$music->getView());
+    }
+
+    /**
+     * @cover ::setView
+     */
+    public function testSetView(){
+
+        self::$music->setView(42);
+        $this->assertSame(42, self::$music->getView());
+
+    }
+
+    /**
+     * @cover ::addView
+     */
+    public function testAddView(){
+
+        self::$music->addView();
+        $this->assertSame(43, self::$music->getView());
+
+    }
 }
