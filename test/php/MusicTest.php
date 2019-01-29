c5f8ca926ca04175c3c960b60861dc5f4cb0cfc4
laurent <djidane56@gmail.com>
2019-01-29 14:15:25 +0100

    test MusicTest

--- test/php/MusicTest.php
+++ test/php/MusicTest.php
@@ -10,13 +10,69 @@ use src\models\Music;
  */
 final class MusicTest extends TestCase
 {
+    private $music;
 
     /**
-     * @cover ::getPath
+     * @beforeClass
+     */
+    protected function setUpNeeds(){
+
+    $music = new Music("J'ai mal au mic","Oxmo Puccino");
+
+    }
+
+    /**
+     * @cover ::getId
      */
     public function testGetId(){
 
-        $music = new Music("J'ai mal au mic","Oxmo Puccino");
         $this->assertSame(0, $music->getId());
     }
+
+    /**
+     * @cover ::setId
+     */
+    public function testSetId(){
+
+        $music->setId(42);
+        $this->assertSame(42, $music->getId());
+
+    }
+
+    /**
+     * @cover ::getTitle
+     */
+    public function testGetTitle(){
+
+        $this->assertSame("J'ai mal au mic", $music->getTitle());
+    }
+
+    /**
+     * @cover ::setTitle
+     */
+    public function testSetTitle(){
+
+        $music->setTitle("Autre titre");
+        $this->assertSame("Autre titre", $music->getTitle());
+
+    }
+
+    /**
+     * @cover ::getArtist
+     */
+    public function testGetArtist(){
+
+        $this->assertSame("Oxmo Puccino", $music->getArtist());
+    }
+
+    /**
+     * @cover ::setArtist
+     */
+    public function testSetArtist(){
+
+        $music->setArtist("Autre artiste");
+        $this->assertSame("Autre artiste", $music->getArtist());
+
+    }
+    
 }
