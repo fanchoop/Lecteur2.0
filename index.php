1e77fe6ee9eefe501f07e587384223b26a56ce29
M-Leveque <leveque.melvin@gmail.com>
2019-02-06 15:25:54 +0100

    - PHP : Index : Page index par défaut

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include_once "src/models/HtmlDocument.php";
 use src\models\HtmlDocument;
-!empty($_GET['page']) ? $page = $_GET['page'] : $page = null;
+!empty($_GET['page']) ? $page = $_GET['page'] : $page = "player";
 
 $html = new HtmlDocument($page);
 $html->render();
